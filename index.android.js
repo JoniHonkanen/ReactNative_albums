8d96577d08eb0d28fe657dd071280b0d938f1f23
Joni Honkanen <joge.honkanen@gmail.com>
2017-07-19 13:28:20 +0300

    kesken

---
+++ index.android.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, AppRegistry, View } from 'react-native';
+
+import Header from './src/components/header';
+import AlbumList from './src/components/albumList';
+// Create a component
+const App = () => {
+  return (
+    <View>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
+  );
+};
+// Render it to the device
+
+
+AppRegistry.registerComponent('albums', () => App);
